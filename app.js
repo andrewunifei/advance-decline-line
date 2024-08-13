16530b5a675d7582ddbc7771b07d1ac42e1baeff
Andrew <andrewunifei@gmail.com>
2024-08-12 23:12:39 -0300

    [ADD] Advance Decline Line

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const start = (timeframe_config) => {
+import fetchChange from "./requests.js";
+
+const start = async (timeframe_config) => {
     const start = Date.now();
     const timeframe = timeframe_config * 60000;
     console.log(timeframe)
     while(1) {
         const relative_moment = Date.now();
         if(relative_moment - start >= timeframe) {
-            console.log('Hello')
+            const data = await fetchChange();
+            console.log(data);
+            break;
         }
     }
 }
 
-const timeframe_config = 1;
-start(timeframe_config);
+const timeframe_config = 0.25;
+console.log(`Timeframe: ${1 * timeframe_config} minutes`)
+await start(timeframe_config);
--- requests.js
+++ requests.js
@@ -1,38 +1,52 @@
 import axios from 'axios';
 
-const getChanges = async () => {
-    try {
-        const res = await axios.get(`https://fapi.binance.com/fapi/v1/ticker/24hr`, {
-            headers: {
-                'content-type': 'application/json',
-            }
-        });
-        return res.data;
-
+const fetchChange = async () => {
+    const getChanges = async () => {
+        try {
+            const res = await axios.get(`https://fapi.binance.com/fapi/v1/ticker/24hr`, {
+                headers: {
+                    'content-type': 'application/json',
+                }
+            });
+            return res.data;
+    
+        }
+        catch(e) {
+            console.log(e);
+        }
     }
-    catch(e) {
-        console.log(e);
+    const price_changes = await getChanges();
+    let losers_accum = 0;
+    let winners_accum = 0;
+    
+    for(let i = 0; i < price_changes.length; i++) {
+        if(price_changes[i].priceChangePercent >= 0) {
+            winners_accum++;
+        }
+        else {
+            losers_accum++;
+        }
+    }
+    
+    const advance_decline_line = winners_accum - losers_accum;
+    const total_binance_futures_assets = winners_accum + losers_accum;
+
+    return {
+        adl: advance_decline_line,
+        total_assets: total_binance_futures_assets,
+        winners_accum,
+        losers_accum
     }
 }
-const price_changes = await getChanges();
-let losers_accum = 0;
-let winners_accum = 0;
 
-for(let i = 0; i < price_changes.length; i++) {
-    if(price_changes[i].priceChangePercent >= 0) {
-        winners_accum++;
-    }
-    else {
-        losers_accum++;
-    }
-}
+export default fetchChange;
 
-const advance_decline_line = winners_accum - losers_accum;
 
-console.log('');
-console.log(`Total 24hr POSITIVE percent change: ${winners_accum}`);
-console.log(`Total 24hr NEGATIVE percent change: ${losers_accum}`);
-console.log(`Total Binance Futures Assets: ${winners_accum + losers_accum}`);
-console.log('');
-console.log(`Advance Decline Line: ${advance_decline_line} `);
-console.log('');
+
+// console.log('');
+// console.log(`Total 24hr POSITIVE percent change: ${winners_accum}`);
+// console.log(`Total 24hr NEGATIVE percent change: ${losers_accum}`);
+// console.log(`Total Binance Futures Assets: ${winners_accum + losers_accum}`);
+// console.log('');
+// console.log(`Advance Decline Line: ${advance_decline_line} `);
+// console.log('');
